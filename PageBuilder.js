50fcb0927120c90ee453db9274607b35522380e9
Samuel Howell <33074970+sahowell117@users.noreply.github.com>
2018-06-10 09:11:57 -0500

    Update PageBuilder.js

--- PageBuilder.js
+++ PageBuilder.js
@@ -3,5 +3,5 @@ function buildPage() {
     html = '<p>test</p>'; 
     //Build the html through the function
     //In the end...
-    $('html-id').empty().append(html);
+    ('html-id').empty().append(html);
 }
