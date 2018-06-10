4b2f442a28ff38af798fa6320017b05427537fae
Samuel Howell <33074970+sahowell117@users.noreply.github.com>
2018-06-10 09:57:44 -0500

    Update and rename PageBuilder.js to PageBuilder.html

--- PageBuilder.js
+++
@@ -1,8 +0,0 @@
-function buildPage() {
-    var html = '<a href =google.com>test</a>';
-   
-    //Build the html through the function
-    //In the end...
-    ('html-id').empty().append(html);
-}
-
